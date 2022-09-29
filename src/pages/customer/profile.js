9aab18f7a9557317743513b20e960b44774642c5
Taufan2992 <taufan.inggil@gmail.com>
2022-09-29 19:03:33 +0700

    update

--- src/pages/customer/profile.js
+++ src/pages/customer/profile.js
@@ -107,7 +107,6 @@ function Profile() {
                         <div class="col-sm-5 pt-3 ps-3 pe-0">
                           <img
                             src={
-                              "https://waysbean.herokuapp.com/uploads/" +
                               data?.product?.image
                             }
                             class="card-img-top"
